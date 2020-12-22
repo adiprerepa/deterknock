abae6d0510353d21a2fc292d71b4d03b014fcf10
Aditya Prerepa <adiprerepa@gmail.com>
2020-12-21 22:33:13 -0800

    final esp

--- door-meeting/door-meeting.ino
+++ door-meeting/door-meeting.ino
@@ -11,20 +11,39 @@
 
 // create an LCD object (Hex address, # characters, # rows)
 // my LCD display in on Hex address 27 and is a 20x4 version
-LiquidCrystal_I2C lcd(0x27, 20, 4); 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 ESP8266WiFiMulti wifiMulti;
 const char* ssid = "onhub";
 const char* pass = "ekchotaghar";
 const int serverPort = 8080;
-const int greenLed = 12;
-const int yellowLed = 13;
-const int redLed = 14;
+const int greenPin = 12;
+const int redPin = 13;
+const int bluePin = 14;
 
 String curStatStr;
 int statSize;
+boolean curBlinkState;
 Thread lcdRotateThread = Thread();
-
+Thread ledBlinkThread = Thread();
+// red -> 255, 0, 0
+// green -> 0, 255, 0
+// blue -> 0, 0, 255
 ESP8266WebServer server(8080);
+
+void writeRgb(int red, int green, int blue) {
+  analogWrite(redPin, red);
+  analogWrite(bluePin, blue);
+  analogWrite(greenPin, green);
+}
+
+void toggleRed() {
+ curBlinkState = !curBlinkState;
+ if (curBlinkState) {
+  writeRgb(255, 0, 0);
+ } else {
+  writeRgb(0, 0, 0);
+ }
+}
 
 void rotateLcd() {
   lcd.setCursor(0, 0);
@@ -34,19 +53,23 @@ void rotateLcd() {
   // rotate array
   char tmp = curStat[0];
   for (int i = 1; i < statSize; i++) {
-    curStat[i-1] = curStat[i];
+    curStat[i - 1] = curStat[i];
   }
   curStat[statSize - 1] = tmp;
   curStatStr = String(curStat);
 }
 
 void setStatus(String lcdStat, String priority) {
+  lcd.clear();
   if (lcdStat.length() > 16) {
-    curStatStr = lcdStat;
+    curStatStr = lcdStat + " ";
+    statSize = curStatStr.length();
     lcdRotateThread.enabled = true;
-    lcd.setCursor(1, 0);
-    lcd.print(priority);
+    Serial.println("enabled lcd thread");
+    lcd.setCursor(0, 1);
+    lcd.print("Priority: " + priority);
   } else {
+    lcdRotateThread.enabled = false;
     lcd.setCursor(0, 0);
     Serial.println(lcdStat);
     lcd.print(lcdStat);
@@ -54,6 +77,19 @@ void setStatus(String lcdStat, String priority) {
     lcd.setCursor(0, 1);
     lcd.print("Priority: " + String(priority));
   }
+  int p = priority.toInt();
+  if (p == 1) {
+    ledBlinkThread.enabled = true;
+    return;
+  } else if (p == 2) {
+    writeRgb(255, 0, 0);
+  } else if (p == 3) {
+    writeRgb(255, 255, 0);
+  } else if (p == 4) {
+    writeRgb(0, 255, 0);
+  }
+  ledBlinkThread.enabled = false;
+
 }
 
 void postStateChange() {
@@ -70,14 +106,12 @@ void postStateChange() {
     return;
   }
   JsonObject obj = doc.as<JsonObject>();
-  Serial.print(F("method: "));
-  Serial.println(server.method());
   if (!server.method() == HTTP_POST) {
     server.send(501, F("application/json"), "{}");
     return;
   }
   if (!(obj.containsKey("lcd_msg") && obj.containsKey("priority"))) {
-    doc["message"] = F("data fields need to include 'lcd_msg' and 'priority'");
+    doc["message"] = F("data fields need to include tw'lcd_msg' and 'priority'");
     String buf;
     serializeJson(doc, buf);
     server.send(400, F("application/json"), buf);
@@ -105,7 +139,7 @@ void setup() {
   Serial.println(WiFi.SSID());
   Serial.print("address:\t");
   Serial.println(WiFi.localIP());
-  
+
   if (!MDNS.begin("aditya_door")) {
     Serial.println("Err setting up MDNS responder");
   }
@@ -113,27 +147,34 @@ void setup() {
   lcdRotateThread.enabled = false;
   lcdRotateThread.onRun(rotateLcd);
   lcdRotateThread.setInterval(1500);
+  ledBlinkThread.enabled = false;
+  ledBlinkThread.setInterval(750);
+  ledBlinkThread.onRun(toggleRed);
+  curBlinkState = false;
   server.on("/", HTTP_GET, []() {
-        server.send(200, F("text/html"),
-            F("Welcome to the REST Web Server"));
-    });
+    server.send(200, F("text/html"),
+                F("Stop knocking on my door"));
+  });
   server.on(F("/stateChange"), HTTP_POST, postStateChange);
   server.begin();
 }
 
 /*
- * Flow:
- * save current state globally
- * mdns.update()
- * display current state on screen/leds
- * wait for state change
- * async check for 
- */
+   Flow:
+   save current state globally
+   mdns.update()
+   display current state on screen/leds
+   wait for state change
+   async check for
+*/
 
-void loop(){
+void loop() {
+  MDNS.update();
+  server.handleClient();
   if (lcdRotateThread.shouldRun()) {
     lcdRotateThread.run();
   }
-  MDNS.update();
-  server.handleClient();
+  if (ledBlinkThread.shouldRun()) {
+    ledBlinkThread.run();
+  }
 }
